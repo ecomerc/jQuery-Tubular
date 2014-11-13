9b0bd5572262402c599cbce0858d0033638bb1d1
petero-dk <peter@ecomerc.com>
2014-11-13 11:25:51 +0100

    Relative, restart with sound
    Ability to position relative on page, restart with sound.

--- src/js/jquery-tubular.js
+++ src/js/jquery-tubular.js
@@ -1,14 +1,21 @@
+/* jQuery tubular plugin
+|* by Sean McCambridge
+|* http://www.seanmccambridge.com/tubular
+|* version: 1.0
+|* updated: October 1, 2012
+|* since 2010
+|* licensed under the MIT License
+|* Enjoy.
+|*
+|* Thanks,
+|* Sean */
 
-(function ($) {
-    'use strict';
+;(function ($, window) {
 
-
-    var Tubular = function (element, options) {
-        this.options = options;
-        this.$element = $(element);
-    }; // Tubular
-
-    Tubular.DEFAULTS = {
+    // test for feature support and return if failure
+   
+    // defaults
+    var defaults = {
         ratio: 16/9, // usually either 4/3 or 16/9 -- tweak as needed
         videoId: 'ZCAnLxRvNNc', // toy robot in space is a good default, no?
         mute: true,
@@ -20,39 +27,146 @@
         muteButtonClass: 'tubular-mute',
         volumeUpClass: 'tubular-volume-up',
         volumeDownClass: 'tubular-volume-down',
+        restartWithSoundClass: 'tubular-restart-withsound',
         increaseVolumeBy: 10,
-        start: 0,
-        callback: null
-    }; // DEFAULTS
+        fixed: true,
+        start: 0
+    };
 
-    Tubular.prototype = {}; // Prototypes
+    // methods
 
-    var old = $.fn.tubular;
+    var tubular = function(node, options) { // should be called on the wrapper div
+        var options = $.extend({}, defaults, options),
+            $body = $('body'), // cache body node
+            $node = $(node), // cache wrapper node
+            $container = $node.parent();
 
-    $.fn.tubular = function (option) {
-        return this.each(function () {
-            var $this = $(this),
-                data = $this.data('.tubular'),
-                options = $.extend({}, Tubular.DEFAULTS, $this.data(), typeof option === 'object' && option);
+        var posYT = options.fixed ? "fixed" : "absolute";
+            
+        // build container
+        var tubularContainer = '<div id="tubular-container" style="overflow: hidden; position: ' + posYT + '; z-index: 1; width: 100%; height: 100%"><div id="tubular-player" style="position: absolute"></div></div><div id="tubular-shield" style="width: 100%; height: 100%; z-index: 2; position: absolute; left: 0; top: 0;"></div>';
 
-            if (!data) {
-                $this.data('.tubular', (data = new Tubular(this, options)));
+        // set up css prereq's, inject tubular container and set up wrapper defaults
+        $('html,body').css({'width': '100%', 'height': '100%'});
+        $container.css({'width': '100%', 'height': '100%'});
+        $container.prepend(tubularContainer);
+        $node.css({position: 'relative', 'z-index': options.wrapperZIndex});
+        if (!options.fixed) {
+            $container.css({position: 'relative', 'z-index': 0});
+        }
+        
+        // set up iframe player, use global scope so YT api can talk
+        window.player;
+        console.log("Hello WORLD");
+        window.onYouTubeIframeAPIReady = function() {
+            player = new YT.Player('tubular-player', {
+                width: options.width,
+                height: Math.ceil(options.width / options.ratio),
+                videoId: options.videoId,
+                playerVars: {
+                    controls: 0,
+                    showinfo: 0,
+                    modestbranding: 1,
+                    wmode: 'transparent'
+                },
+                events: {
+                    'onReady': onPlayerReady,
+                    'onStateChange': onPlayerStateChange
+                }
+            });
+        }
+
+        window.onPlayerReady = function(e) {
+            resize();
+            if (options.mute) e.target.mute();
+            e.target.seekTo(options.start);
+            e.target.playVideo();
+        }
+
+        window.onPlayerStateChange = function(state) {
+            if (state.data === 0 && options.repeat) { // video ended and repeat option is set true
+                player.seekTo(options.start); // restart
+            }
+        }
+        
+
+        // resize handler updates width, height and offset of player after resize/init
+        var resize = function() {
+            var width = $container.width(),
+                pWidth, // player width, to be defined
+                height = $container.height(),
+                pHeight, // player height, tbd
+                $tubularPlayer = $('#tubular-player');
+
+            // when screen aspect ratio differs from video, video must center and underlay one dimension
+
+            if (width / options.ratio < height) { // if new video height < window height (gap underneath)
+                pWidth = Math.ceil(height * options.ratio); // get new player width
+                $tubularPlayer.width(pWidth).height(height).css({left: (width - pWidth) / 2, top: 0}); // player width is greater, offset left; reset top
+            } else { // new video width < window width (gap to right)
+                pHeight = Math.ceil(width / options.ratio); // get new player height
+                $tubularPlayer.width(width).height(pHeight).css({left: 0, top: (height - pHeight) / 2}); // player height is greater, offset top; reset left
             }
 
-            if (typeof option === 'string') {
-                data[option]();
+        }
+
+        // events
+        $(window).on('resize.tubular', function() {
+            resize();
+        })
+
+        $('body').on('click','.' + options.playButtonClass, function(e) { // play button
+            e.preventDefault();
+            player.playVideo();
+        }).on('click', '.' + options.pauseButtonClass, function(e) { // pause button
+            e.preventDefault();
+            player.pauseVideo();
+        }).on('click', '.' + options.restartWithSoundClass, function(e) { // restart with sound
+            e.preventDefault();
+            if (player.isMuted()) {
+                player.pauseVideo();
+                player.unMute();
+                player.seekTo(options.start); // restart
+                player.playVideo();
+            } else {
+                (player.isMuted()) ? player.unMute() : player.mute();
+            }
+        }).on('click', '.' + options.muteButtonClass, function(e) { // mute button
+            e.preventDefault();
+            (player.isMuted()) ? player.unMute() : player.mute();
+        }).on('click', '.' + options.volumeDownClass, function(e) { // volume down button
+            e.preventDefault();
+            var currentVolume = player.getVolume();
+            if (currentVolume < options.increaseVolumeBy) currentVolume = options.increaseVolumeBy;
+            player.setVolume(currentVolume - options.increaseVolumeBy);
+        }).on('click', '.' + options.volumeUpClass, function(e) { // volume up button
+            e.preventDefault();
+            if (player.isMuted()) player.unMute(); // if mute is on, unmute
+            var currentVolume = player.getVolume();
+            if (currentVolume > 100 - options.increaseVolumeBy) currentVolume = 100 - options.increaseVolumeBy;
+            player.setVolume(currentVolume + options.increaseVolumeBy);
+        })
+        
+        
+        
+        // load yt iframe js api
+
+        var tag = document.createElement('script');
+        tag.src = "//www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    }
+
+
+    // create plugin
+
+    $.fn.tubular = function (options) {
+        return this.each(function () {
+            if (!$.data(this, 'tubular_instantiated')) { // let's only run one
+                $.data(this, 'tubular_instantiated',
+                tubular(this, options));
             }
         });
-    }; // $.fn.tubular
+    }
 
-    $.fn.tubular.Constructor = Tubular;
-
-    $.fn.tubular.noConflict = function () {
-        $.fn.tubular = old;
-        return this;
-    }; // No conflict
-
-    $(document)
-    .on('', '', function () {});
-
-}) (window.jQuery);
+})(jQuery, window);
